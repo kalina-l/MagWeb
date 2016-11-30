f243f099f66167b2fea0ebf06ded91a4e2d6a589
kalina <kalina.lebecka@gmail.com>
2016-11-30 12:07:36 +0100

    basic app structure

---
+++ app.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var stylus = require('stylus');
+
+var app = express();
+
+function compile(str, path) {
+   return stylus(str)
+     .set('filename', path)
+     .set('compress', true)
+     .use(nib())
+     .import('nib');
+ }
+
+app.set('views', './templates');
+app.set('view engine', 'pug');
+app.use(stylus.middleware(
+  { src: __dirname + '/public/styles'
+   , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+require('./router')(app);
+
+app.listen(8080, function () {
+  console.log('Server listen on port 8080')
+});
