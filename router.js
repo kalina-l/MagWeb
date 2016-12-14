1132ffad2228160b46be88eb3089a03455293a0a
kalina <kalina.lebecka@gmail.com>
2016-12-14 15:26:35 +0100

    basic front page design

--- router.js
+++ router.js
@@ -2,4 +2,9 @@ var controller = require('./controller');
 
 module.exports = function (app) {
   app.get('/', controller.indexAction);
+  app.get('/:category?/:article?', controller.indexAction);
+  app.get('/', controller.indexAction);
+  app.get('/', controller.indexAction);
+  app.get('/', controller.indexAction);
+  app.get('/', controller.indexAction);
 };
